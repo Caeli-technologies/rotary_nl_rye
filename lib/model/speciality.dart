6d64a587372b4bcef8d7cb3759a687428ff56fa2
Ruben Talstra <RubenTalstra1211@outlook.com>
2021-04-21 15:36:57 +0200

    update

---
+++ lib/model/speciality.dart
@@ -0,0 +1,14 @@
+// @dart=2.9
+import 'package:flutter/cupertino.dart';
+
+class SpecialityModel {
+  String imgAssetPath;
+  String speciality;
+  int noOfDoctors;
+  Color backgroundColor;
+  SpecialityModel(
+      {this.imgAssetPath,
+      this.speciality,
+      this.noOfDoctors,
+      this.backgroundColor});
+}
