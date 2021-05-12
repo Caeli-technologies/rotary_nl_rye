6b6c81d6a4c0f41722fbc049d4b1a03dbe1b45db
wick3ds0ul <beelzabub.9@gmail.com>
2021-05-12 10:13:40 +0530

    Added Contact person and details.

--- lib/features/stories/presentation/pages/contact_page.dart
+++ lib/features/stories/presentation/pages/contact_page.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rotary_nl_rye/core/prop.dart';
+import 'package:rotary_nl_rye/features/stories/presentation/pages/contact_person_details_page.dart';
 
 class ContactPage extends StatefulWidget {
   @override
@@ -26,47 +27,159 @@ class _ContactPageState extends State<ContactPage> {
                   TextStyle(color: Palette.indigo, fontWeight: FontWeight.bold),
             ),
           ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 18, vertical: 8),
-            child: ListTile(
-              contentPadding: EdgeInsets.all(0),
-              leading: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(40)),
-                child: Container(
-                  height: 55,
-                  width: 55,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(40),
-                    color: Palette
-                        .imageBackgroundColor, //fill the image still needs to chagnge
-                  ),
-                  child: Image.asset(
-                    'assets/image/1.PNG',
-                    height: 50,
-                    width: 50,
-                    fit: BoxFit.contain,
-                  ),
-                ),
-              ),
-              title: Text('Ruben Talstra',
-                  style: TextStyle(
-                    color: Palette.indigo,
-                  )),
-              subtitle: Text(
-                'My fuction XD is a Dev i think',
-                style: TextStyle(
-                  color: Palette.indigo,
-                ),
-              ),
-              trailing: Icon(
-                Icons.keyboard_arrow_right,
-                size: 30,
-                color: Palette.indigo,
-              ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemBuilder: (context, index) => PersonTile(
+              person: personList[index],
             ),
+            itemCount: personList.length,
           )
         ],
       ),
     );
   }
 }
+
+class PersonTile extends StatelessWidget {
+  final Person person;
+  const PersonTile({
+    this.person,
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 18, vertical: 8),
+      child: ListTile(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => PersonDetails(person: person)),
+          );
+        },
+        contentPadding: EdgeInsets.all(0),
+        leading: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(40)),
+          child: Container(
+            height: 55,
+            width: 55,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(40),
+              color: Palette
+                  .imageBackgroundColor, //fill the image still needs to chagnge
+            ),
+            child: Image.asset(
+              person.imageUrl,
+              height: 50,
+              width: 50,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+        title: Text(person.name,
+            style: TextStyle(
+              color: Palette.indigo,
+            )),
+        subtitle: Text(
+          person.role,
+          style: TextStyle(
+            color: Palette.indigo,
+          ),
+        ),
+        trailing: Icon(
+          Icons.keyboard_arrow_right,
+          size: 30,
+          color: Palette.indigo,
+        ),
+      ),
+    );
+  }
+}
+
+//TODO:This needs to be in other file.
+//Mock Data
+class Person {
+  final String name;
+  final String role;
+  final String bio;
+  final String place;
+  final String imageUrl;
+  final String email;
+  final String phoneNumber;
+  final Map<String, dynamic> exchangeInfo;
+  Person(
+      {this.name,
+      this.role,
+      this.bio,
+      this.place,
+      this.imageUrl,
+      this.email,
+      this.phoneNumber,
+      this.exchangeInfo});
+}
+
+List<Person> personList = [
+  Person(
+      name: "Ruben Talstra",
+      role: "Flutter Dev",
+      place: "Netherlands",
+      bio:
+          "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+      imageUrl: "assets/image/1.PNG",
+      email: "ruben@example.com",
+      phoneNumber: "888 444 7676",
+      exchangeInfo: {
+        "place": "Japan",
+        "travelDates": ["March 2020", "May 2020"]
+      }),
+  Person(
+      name: "_Bnkn_",
+      role: "Flutter Dev",
+      place: "U.S",
+      bio:
+          "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+      imageUrl: "assets/image/2.PNG",
+      email: "bnkn@example.com",
+      phoneNumber: "888 444 7676",
+      exchangeInfo: {
+        "place": "Japan",
+        "travelDates": ["March 2020", "May 2020"]
+      }),
+  Person(
+      name: "Sceptile",
+      role: "Flutter Dev",
+      place: "Germany",
+      bio:
+          "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+      imageUrl: "assets/image/3.PNG",
+      email: "sceptile@example.com",
+      phoneNumber: "888 444 7676",
+      exchangeInfo: {
+        "place": "Japan",
+        "travelDates": ["March 2020", "May 2020"]
+      }),
+  Person(
+      name: "Ton Ann",
+      role: "Flutter Dev",
+      place: "France",
+      bio:
+          "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+      imageUrl: "assets/image/1.PNG",
+      email: "tonann@example.com",
+      phoneNumber: "888 444 7676",
+      exchangeInfo: {
+        "place": "Japan",
+        "travelDates": ["March 2020", "May 2020"]
+      }),
+  Person(
+      name: "Believer",
+      role: "Flutter Dev",
+      place: "India",
+      bio:
+          "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+      imageUrl: "assets/image/2.PNG",
+      email: "believer@example.com",
+      phoneNumber: "888 444 7676",
+      exchangeInfo: {}),
+];
---
+++ lib/features/stories/presentation/pages/contact_person_details_page.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:rotary_nl_rye/core/prop.dart';
+import 'contact_page.dart';
+
+class PersonDetails extends StatelessWidget {
+  final Person person;
+  const PersonDetails({required this.person});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: Container(
+          margin: EdgeInsets.only(left: 10, top: 10),
+          width: 40,
+          height: 40,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(40.0)),
+          child: RawMaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: new Icon(
+              Icons.arrow_back,
+              color: Palette.accentColor,
+              size: 30.0,
+            ),
+            shape: new CircleBorder(),
+            elevation: 2.0,
+            fillColor: Palette.themeShadeColor,
+            padding: const EdgeInsets.all(5.0),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(left: 15, right: 15),
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              Container(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage(person.imageUrl),
+                  radius: 125,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                person.name,
+                textAlign: TextAlign.center,
+                textScaleFactor: 2,
+                style: TextStyle(
+                  color: Palette.indigo,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              IntrinsicHeight(
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text(
+                    person.role,
+                    textAlign: TextAlign.center,
+                  ),
+                  VerticalDivider(
+                    thickness: 2,
+                    width: 20,
+                    color: Colors.grey,
+                  ),
+                  Text(
+                    person.place,
+                    textAlign: TextAlign.center,
+                  )
+                ]),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.phone),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(person.phoneNumber)
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.email),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(person.email)
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(person.bio),
+              SizedBox(
+                height: 20,
+              ),
+              person.exchangeInfo.length == 0
+                  ? SizedBox.shrink()
+                  : Text(
+                      "Travelled to: ${person.exchangeInfo["place"]} between ${person.exchangeInfo["travelDates"][0]}-${person.exchangeInfo["travelDates"][1]}")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
