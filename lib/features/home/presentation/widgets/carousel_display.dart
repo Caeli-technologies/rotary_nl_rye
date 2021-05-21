6bf727629265660f38f3c1aadfdba11293914a9d
Ruben Talstra <RubenTalstra1211@outlook.com>
2021-05-21 17:45:16 +0200

    adding null safety

--- lib/features/home/presentation/widgets/carousel_display.dart
+++ lib/features/home/presentation/widgets/carousel_display.dart
@@ -1,7 +1,6 @@
-// @dart=2.9
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 
 import '../../../../core/prop.dart';
 
@@ -118,5 +117,5 @@ class _CarouselState extends State<Carousel> {
 class CarouselModel {
   final String image, text;
 
-  CarouselModel({this.image, this.text});
+  CarouselModel({required this.image, required this.text});
 }
