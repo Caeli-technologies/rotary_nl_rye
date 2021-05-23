09d1dbe7e48132901179ac1f304daa5bf8d696e9
Ruben Talstra <RubenTalstra1211@outlook.com>
2021-05-23 10:34:55 +0200

    fix bug in code. don't use "pedantic" yet

--- lib/core/prop.dart
+++ lib/core/prop.dart
@@ -11,29 +11,29 @@ class Device {
   static bool isDark = false;
 
   static String convert(int millisecondsSinceEpoch) {
-    String result = '';
+    String result = "";
 
     List months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'September',
-      'October',
-      'November',
-      'December'
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "September",
+      "October",
+      "November",
+      "December"
     ];
 
     result += DateTime.fromMillisecondsSinceEpoch(millisecondsSinceEpoch)
         .day
         .toString();
-    result += ' ';
+    result += " ";
     result += months[
         DateTime.fromMillisecondsSinceEpoch(millisecondsSinceEpoch).month - 1];
-    result += ' ';
+    result += " ";
     result += DateTime.fromMillisecondsSinceEpoch(millisecondsSinceEpoch)
         .year
         .toString();
@@ -56,15 +56,15 @@ class Palette {
   static Color imageBackgroundColor = 0 as Color;
 
   static Palette get instance {
-    _instance.set;
+    _instance.set();
     return _instance;
   }
 
-  void set(){
-    accentColor = const Color.fromRGBO(57, 182, 245, 1);
+  set() {
+    accentColor = Color.fromRGBO(57, 182, 245, 1);
     grey = Colors.grey[600]!;
     lightIndigo = Colors.indigo[100]!;
-    imageBackgroundColor = const Color.fromRGBO(70, 197, 212, 1);
+    imageBackgroundColor = Color.fromRGBO(70, 197, 212, 1);
 
     if (Device.isDark) {
       indigo = Colors.indigo[400]!;
