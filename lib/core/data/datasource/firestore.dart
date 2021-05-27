4c572012bbf2e15adf683d741cb3d3fa1aade131
Jannik Norden <dev.janniknorden@gmail.com>
2021-05-27 17:07:09 +0200

    Firebase comments added

--- lib/core/data/datasource/firestore.dart
+++ lib/core/data/datasource/firestore.dart
@@ -8,11 +8,18 @@ class FbRemote {
   Future<List> get() async {
     List<QueryDocumentSnapshot> documents = [];
 
+    // TODO fetch changes from remote
     try {
+      // it should only fetch changed documents from firebase and apply the changes to the cache
+      // https://firebase.google.com/docs/firestore/query-data/listen)
+
+      // fetches all documents from local
       await FirebaseFirestore.instance.collection(collection).orderBy('name').get(GetOptions(source: Source.cache)).then((QuerySnapshot querySnapshot) {
         documents = querySnapshot.docs;
         print('from cache');
       });
+
+      // fetches all documents from remote if cache is empty
       if (documents.toString() == '[]') {
         await FirebaseFirestore.instance.collection(collection).orderBy('name').get(GetOptions(source: Source.server)).then((QuerySnapshot querySnapshot) {
           documents = querySnapshot.docs;
