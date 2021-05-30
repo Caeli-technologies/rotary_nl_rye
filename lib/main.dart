eb768205e32966319355537aaa68279c3414f32d
Ruben Talstra <RubenTalstra1211@outlook.com>
2021-05-30 09:44:00 +0200

    ios is working

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // @dart=2.9
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/services.dart';
@@ -12,6 +13,7 @@ import 'injection_container.dart' as di;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  // FirebaseCrashlytics.instance.crash();
   await di.init();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
