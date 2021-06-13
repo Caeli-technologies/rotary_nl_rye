dadaa05a8cf85b8cf478d354a603b63894d1b73a
Ruben Talstra <RubenTalstra1211@outlook.com>
2021-06-13 21:46:31 +0200

    please test it :)

--- lib/features/news/presentation/widgets/pdf_viewer.dart
+++ lib/features/news/presentation/widgets/pdf_viewer.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 
 import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:rotary_nl_rye/core/prop.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PDFPage extends StatefulWidget {
   final String pdfUrl;
@@ -24,6 +26,10 @@ class _PDFPageState extends State<PDFPage> {
   bool _isLoading = true;
   late PDFDocument document;
   String title = "Loading";
+
+  String? _linkMessage;
+  bool _isCreatingLink = false;
+  String? id;
 
   @override
   void initState() {
@@ -75,23 +81,15 @@ class _PDFPageState extends State<PDFPage> {
               padding: const EdgeInsets.all(5.0),
             ),
           ),
-// TODO not yet working, Dynamic link to share this pdf page with someone else
-
-          // actions: <Widget>[
-          //   IconButton(
-          //     icon: Icon(
-          //       CupertinoIcons.share,
-          //       color: Palette.indigo,
-          //     ),
-          //     onPressed: () {
-          //       // Share.share(
-          //       //     Platform.isIOS
-          //       //         ? 'Hier mot nog een leuk stukje komen. + de link naar de Apple app store link https://example.com'
-          //       //         : 'Hier mot nog een leuk stukje komen. + de link naar de google play store link https://example.com',
-          //       //     subject: 'look at this nice app :)');
-          //     },
-          //   )
-          // ],
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                CupertinoIcons.share,
+                color: Palette.indigo,
+              ),
+              onPressed: _createShareURL,
+            )
+          ],
         ),
         body: Center(
             child: _isLoading
@@ -139,5 +137,55 @@ class _PDFPageState extends State<PDFPage> {
                     );
                   }, */
                   )));
+  }
+
+  Future<void> _createDynamicLink(String id) async {
+    setState(() {
+      _isCreatingLink = true;
+    });
+
+    final DynamicLinkParameters parameters = DynamicLinkParameters(
+      uriPrefix: 'https://rotarytestnl.page.link',
+      link: Uri.parse(
+          'https://rotarytestnl.page.link/news?id=$id'), //change this to the url in the main.dart
+      androidParameters: AndroidParameters(
+        packageName: 'com.caelitechnologies.rotary_nl_rye',
+        minimumVersion: 1,
+      ),
+      iosParameters: IosParameters(
+        bundleId: 'com.caelitechnologies.rotary-nl-rye',
+        minimumVersion: '1',
+        appStoreId: '1567096118',
+      ),
+      // socialMetaTagParameters: SocialMetaTagParameters(
+      //   title: 'Example of a Dynamic Link',
+      //   description: 'This link works whether app is installed or not!',
+      //   imageUrl: Uri.parse(
+      //       'https://is4-ssl.mzstatic.com/image/thumb/Purple114/v4/6e/21/e4/6e21e45b-49cb-fa52-83c2-bb56ab288b49/AppIcon-0-0-1x_U007emarketing-0-0-0-4-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.jpeg/1200x630wa.png'),
+      // ),
+    );
+
+    Uri url;
+    final ShortDynamicLink shortLink = await parameters.buildShortLink();
+    url = shortLink.shortUrl;
+
+    setState(() {
+      _linkMessage = url.toString();
+      _isCreatingLink = false;
+    });
+  }
+
+  _createShareURL() async {
+    _createDynamicLink(id = 'id_test_1');
+
+    if (await canLaunch(_linkMessage!)) {
+      await Share.share(
+          Platform.isIOS
+              ? 'Hier mot nog een leuk stukje komen. + de link naar de juiste pagina $_linkMessage' // iOS
+              : 'Hier mot nog een leuk stukje komen. + de link naar de juiste pagina $_linkMessage', //android
+          subject: 'look at this nice app :)');
+    } else {
+      throw 'Could not launch $_linkMessage';
+    }
   }
 }
