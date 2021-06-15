98f9556b9d9702674cbd5a24286b8056999d3271
Ruben Talstra <RubenTalstra1211@outlook.com>
2021-06-15 09:09:06 +0200

    let's go working background notifications + path

--- lib/core/presentation/widgets/page_navigator.dart
+++ lib/core/presentation/widgets/page_navigator.dart
@@ -37,6 +37,30 @@ class _PageNavigatorState extends State<PageNavigator> {
       print(token); // Print the Token in Console
     });
 
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage? message) async {
+      if (message?.data["navigation"] == "/news") {
+        String id = message?.data["id"];
+        print('news id $id');
+        List<News> _newsList = await _repo.fetchNews();
+        print('news fetched ${_newsList[int.parse(id)].toString()}');
+        _newsList[int.parse(id)].isPdf
+            ? Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => PDFPage(
+                        pdfId: _newsList[int.parse(id)],
+                        pdfUrl: _newsList[int.parse(id)].pdf!)),
+              )
+            : Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        NonPDFPage(data: _newsList[int.parse(id)])));
+      }
+    });
+
     FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
       RemoteNotification? notification = message.notification;
       AndroidNotification? android = message.notification?.android;
@@ -57,25 +81,49 @@ class _PageNavigatorState extends State<PageNavigator> {
                     0xFFf7a81b), // TODO still need to change to a better one
               ),
             ));
-        if (message.data["navigation"] == "/news") {
-          String id = message.data["id"];
-          print('news id $id');
-          List<News> _newsList = await _repo.fetchNews();
-          print('news fetched ${_newsList[int.parse(id)].toString()}');
-          _newsList[int.parse(id)].isPdf
-              ? Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => PDFPage(
-                          pdfId: _newsList[int.parse(id)],
-                          pdfUrl: _newsList[int.parse(id)].pdf!)),
-                )
-              : Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          NonPDFPage(data: _newsList[int.parse(id)])));
-        }
+      }
+      // print('A new onMessage event was published!');
+      // if (message.data["navigation"] == "/news") {
+      //   String id = message.data["id"];
+      //   print('news id $id');
+      //   List<News> _newsList = await _repo.fetchNews();
+      //   print('news fetched ${_newsList[int.parse(id)].toString()}');
+      //   _newsList[int.parse(id)].isPdf
+      //       ? Navigator.push(
+      //           context,
+      //           MaterialPageRoute(
+      //               builder: (context) => PDFPage(
+      //                   pdfId: _newsList[int.parse(id)],
+      //                   pdfUrl: _newsList[int.parse(id)].pdf!)),
+      //         )
+      //       : Navigator.push(
+      //           context,
+      //           MaterialPageRoute(
+      //               builder: (context) =>
+      //                   NonPDFPage(data: _newsList[int.parse(id)])));
+      // }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) async {
+      print('A new onMessageOpenedApp event was published!');
+      if (message.data["navigation"] == "/news") {
+        String id = message.data["id"];
+        print('news id $id');
+        List<News> _newsList = await _repo.fetchNews();
+        print('news fetched ${_newsList[int.parse(id)].toString()}');
+        _newsList[int.parse(id)].isPdf
+            ? Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => PDFPage(
+                        pdfId: _newsList[int.parse(id)],
+                        pdfUrl: _newsList[int.parse(id)].pdf!)),
+              )
+            : Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        NonPDFPage(data: _newsList[int.parse(id)])));
       }
     });
 
