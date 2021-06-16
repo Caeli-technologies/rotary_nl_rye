e378e9fb5a9604a49b68aeb89c6deb2b693beb19
Ruben Talstra <RubenTalstra1211@outlook.com>
2021-06-16 11:52:34 +0200

    cleaning up some code :)

---
+++ lib/core/path/firebase_cloud_messaging.dart
@@ -0,0 +1,123 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app_badger/flutter_app_badger.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:rotary_nl_rye/core/bloc/repository.dart';
+import 'package:rotary_nl_rye/features/news/models/firestore_url.dart';
+import 'package:rotary_nl_rye/features/news/models/news.dart';
+import 'package:rotary_nl_rye/features/news/presentation/pages/non_pdf_news.dart';
+import 'package:rotary_nl_rye/features/news/presentation/widgets/pdf_viewer.dart';
+import 'package:rotary_nl_rye/main.dart';
+
+late FireStoreUrl _news;
+String _appBadgeSupported = 'Unknown';
+Repository _repo = Repository();
+
+Future<void> getToken() async {
+  FirebaseMessaging.instance.getToken().then((token) {
+    print(token); // Print the Token in Console
+  });
+}
+
+Future<void> getInitialMessages(BuildContext context) async {
+  FirebaseMessaging.instance
+      .getInitialMessage()
+      .then((RemoteMessage? message) async {
+    if (message?.data["navigation"] == "/news") {
+      _removeBadge();
+      String id = message?.data["id"];
+      print('news id $id');
+      List<News> _newsList = await _repo.fetchNews();
+      print('news fetched ${_newsList[int.parse(id)].toString()}');
+      _newsList[int.parse(id)].isPdf
+          ? Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => PDFPage(
+                      pdfId: _newsList[int.parse(id)],
+                      pdfUrl: _newsList[int.parse(id)].pdf!)),
+            )
+          : Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      NonPDFPage(data: _newsList[int.parse(id)])));
+    }
+  });
+}
+
+Future<void> onMessage(BuildContext context) async {
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+    RemoteNotification? notification = message.notification;
+    AndroidNotification? android = message.notification?.android;
+    if (notification != null && android != null && !kIsWeb) {
+      flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              channel.description,
+              // TODO add a proper drawable resource to android, for now using
+              //      one that already exists in example app.
+              icon: 'ic_stat_rotary_logo_icon',
+              color: Color(
+                  0xFFf7a81b), // TODO still need to change to a better one
+            ),
+          ));
+    }
+    // print('A new onMessage event was published!');
+    // if (message.data["navigation"] == "/news") {
+    //   String id = message.data["id"];
+    //   print('news id $id');
+    //   List<News> _newsList = await _repo.fetchNews();
+    //   print('news fetched ${_newsList[int.parse(id)].toString()}');
+    //   _newsList[int.parse(id)].isPdf
+    //       ? Navigator.push(
+    //           context,
+    //           MaterialPageRoute(
+    //               builder: (context) => PDFPage(
+    //                   pdfId: _newsList[int.parse(id)],
+    //                   pdfUrl: _newsList[int.parse(id)].pdf!)),
+    //         )
+    //       : Navigator.push(
+    //           context,
+    //           MaterialPageRoute(
+    //               builder: (context) =>
+    //                   NonPDFPage(data: _newsList[int.parse(id)])));
+    // }
+  });
+}
+
+Future<void> onMessageOpenedApp(BuildContext context) async {
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) async {
+    print('A new onMessageOpenedApp event was published!');
+    if (message.data["navigation"] == "/news") {
+      String id = message.data["id"];
+      print('news id $id');
+      List<News> _newsList = await _repo.fetchNews();
+      print('news fetched ${_newsList[int.parse(id)].toString()}');
+      _newsList[int.parse(id)].isPdf
+          ? Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => PDFPage(
+                      pdfId: _newsList[int.parse(id)],
+                      pdfUrl: _newsList[int.parse(id)].pdf!)),
+            )
+          : Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      NonPDFPage(data: _newsList[int.parse(id)])));
+    }
+    print('onMessage: ${message.data}');
+  });
+}
+
+void _removeBadge() {
+  FlutterAppBadger.removeBadge();
+}
--- lib/core/presentation/widgets/page_navigator.dart
+++ lib/core/presentation/widgets/page_navigator.dart
@@ -9,6 +9,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:rotary_nl_rye/core/bloc/repository.dart';
 import 'package:rotary_nl_rye/core/data/check_update.dart';
+import 'package:rotary_nl_rye/core/path/firebase_cloud_messaging.dart';
 import 'package:rotary_nl_rye/features/news/models/firestore_url.dart';
 import 'package:rotary_nl_rye/features/news/models/news.dart';
 import 'package:rotary_nl_rye/features/news/presentation/pages/non_pdf_news.dart';
@@ -38,149 +39,13 @@ class _PageNavigatorState extends State<PageNavigator> {
   initState() {
     this._initDynamicLinks();
     appBadgeSupportedState();
+    getToken();
+    getInitialMessages(context);
+    onMessage(context);
+    onMessageOpenedApp(context);
     _removeBadge();
     super.initState();
-  }
-
-  Future<void> getToken() async {
-    FirebaseMessaging.instance.getToken().then((token) {
-      print(token); // Print the Token in Console
-    });
-  }
-
-  Future<void> getInitialMessages() async {
-    FirebaseMessaging.instance
-        .getInitialMessage()
-        .then((RemoteMessage? message) async {
-      if (message?.data["navigation"] == "/news") {
-        _removeBadge();
-        String id = message?.data["id"];
-        print('news id $id');
-        List<News> _newsList = await _repo.fetchNews();
-        print('news fetched ${_newsList[int.parse(id)].toString()}');
-        _newsList[int.parse(id)].isPdf
-            ? Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) =>
-                  PDFPage(
-                      pdfId: _newsList[int.parse(id)],
-                      pdfUrl: _newsList[int.parse(id)].pdf!)),
-        )
-            : Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) =>
-                    NonPDFPage(data: _newsList[int.parse(id)])));
-      }
-    });
-  }
-
-  Future<void> onMessage() async {
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
-      RemoteNotification? notification = message.notification;
-      AndroidNotification? android = message.notification?.android;
-      if (notification != null && android != null && !kIsWeb) {
-        flutterLocalNotificationsPlugin.show(
-            notification.hashCode,
-            notification.title,
-            notification.body,
-            NotificationDetails(
-              android: AndroidNotificationDetails(
-                channel.id,
-                channel.name,
-                channel.description,
-                // TODO add a proper drawable resource to android, for now using
-                //      one that already exists in example app.
-                icon: 'ic_stat_rotary_logo_icon',
-                color: Color(
-                    0xFFf7a81b), // TODO still need to change to a better one
-              ),
-            ));
-      }
-      // print('A new onMessage event was published!');
-      // if (message.data["navigation"] == "/news") {
-      //   String id = message.data["id"];
-      //   print('news id $id');
-      //   List<News> _newsList = await _repo.fetchNews();
-      //   print('news fetched ${_newsList[int.parse(id)].toString()}');
-      //   _newsList[int.parse(id)].isPdf
-      //       ? Navigator.push(
-      //           context,
-      //           MaterialPageRoute(
-      //               builder: (context) => PDFPage(
-      //                   pdfId: _newsList[int.parse(id)],
-      //                   pdfUrl: _newsList[int.parse(id)].pdf!)),
-      //         )
-      //       : Navigator.push(
-      //           context,
-      //           MaterialPageRoute(
-      //               builder: (context) =>
-      //                   NonPDFPage(data: _newsList[int.parse(id)])));
-      // }
-    });
-  }
-
-  Future<void> onMessageOpenedApp() async {
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) async {
-      print('A new onMessageOpenedApp event was published!');
-      if (message.data["navigation"] == "/news") {
-        String id = message.data["id"];
-        print('news id $id');
-        List<News> _newsList = await _repo.fetchNews();
-        print('news fetched ${_newsList[int.parse(id)].toString()}');
-        _newsList[int.parse(id)].isPdf
-            ? Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => PDFPage(
-                        pdfId: _newsList[int.parse(id)],
-                        pdfUrl: _newsList[int.parse(id)].pdf!)),
-              )
-            : Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        NonPDFPage(data: _newsList[int.parse(id)])));
-      }
-      print('onMessage: ${message.data}');
-    });
-
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) async {
-      print("onMessageOpenedApp: $message");
-
-      // if (message.data["navigation"] == "/news") {
-      //   int _yourId = int.tryParse(message.data["id"]) ?? 0;
-      //   Navigator.push(
-      //       navigatorKey.currentState!.context,
-      //       MaterialPageRoute(
-      //           builder: (context) => SocialPage(
-      //                 id: _yourId,
-      //               )));
-      // }
-
-      if (message.data["navigation"] == "/news") {
-        _removeBadge();
-        String id = message.data["id"];
-        print('news id $id');
-        List<News> _newsList = await _repo.fetchNews();
-        print('news fetched ${_newsList[int.parse(id)].toString()}');
-        _newsList[int.parse(id)].isPdf
-            ? Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => PDFPage(
-                        pdfId: _newsList[int.parse(id)],
-                        pdfUrl: _newsList[int.parse(id)].pdf!)),
-              )
-            : Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        NonPDFPage(data: _newsList[int.parse(id)])));
-      }
-    });
-// versionCheck firebase remote config
+    // versionCheck firebase remote config
     try {
       versionCheck(context);
     } catch (e) {
