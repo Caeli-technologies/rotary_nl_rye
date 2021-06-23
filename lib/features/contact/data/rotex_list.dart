0cd8fb84b250e98a8c730dc6f1199b224f12537f
Ruben Talstra <RubenTalstra1211@outlook.com>
2021-06-23 12:23:04 +0200

    fixed

--- lib/features/contact/data/rotex_list.dart
+++ lib/features/contact/data/rotex_list.dart
@@ -6,7 +6,7 @@ List<Rotex> rotexList = [
       name: "Kylian Klein Hesselink",
       bio: "kylianHesselinkBio",
       imageUrl:
-          "https://scontent-amt2-1.xx.fbcdn.net/v/t1.6435-9/52958230_2181457118578153_5472380623979020288_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=v-gUXdkMO0kAX9MXFNw&_nc_ht=scontent-amt2-1.xx&oh=66200dd3bf1948d42f93821d98bb54e9&oe=60CF540C",
+          "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
       email: "ruben@example.com",
       phoneNumber: "888 444 7676",
       facebookUrl: '',
@@ -33,7 +33,7 @@ List<Rotex> rotexList = [
     name: "Céline Hof",
     bio: "celineHofBio",
     imageUrl:
-        "https://media-exp1.licdn.com/dms/image/C4E03AQHfyT-PqTjZUQ/profile-displayphoto-shrink_800_800/0/1620311758692?e=1627516800&v=beta&t=k9bw8OV40CNp3CQmTxGfNmymduN5b-GF9stkF9RkN0s",
+        "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
     email: "sceptile@example.com",
     phoneNumber: "888 444 7676",
     facebookUrl: '',
@@ -47,7 +47,7 @@ List<Rotex> rotexList = [
     name: "Victor Storm van ‘s Gravesande",
     bio: "victorStormBio",
     imageUrl:
-        "https://scontent-amt2-1.xx.fbcdn.net/v/t31.18172-1/c0.33.200.200a/p200x200/14352584_1226960370678873_2501773787135356157_o.jpg?_nc_cat=106&ccb=1-3&_nc_sid=7206a8&_nc_ohc=zlXBzgdl9fsAX8Dysuz&_nc_ht=scontent-amt2-1.xx&tp=27&oh=eb13a374f50e358202a0c3719b2aaa42&oe=60CFFEC6",
+        "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
     email: "tonann@example.com",
     phoneNumber: "888 444 7676",
     facebookUrl: '',
@@ -61,7 +61,7 @@ List<Rotex> rotexList = [
       name: "Fleur Heupers",
       bio: "fleurHeupersBio",
       imageUrl:
-          "https://scontent-ams4-1.xx.fbcdn.net/v/t1.6435-9/95066683_1178177425877097_5547402217897590784_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=2JhSlWD6jPMAX9oDfMx&_nc_ht=scontent-ams4-1.xx&oh=e0ff619ec824135f7261b52e0b78fb41&oe=60CF0513",
+          "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
       email: "believer@example.com",
       phoneNumber: "888 444 7676",
       facebookUrl: '',
@@ -74,7 +74,7 @@ List<Rotex> rotexList = [
       name: "Josephine van Dijk",
       bio: "josephineDijkBio",
       imageUrl:
-          "https://media-exp1.licdn.com/dms/image/C4E03AQGngP9m0NVopA/profile-displayphoto-shrink_800_800/0/1610529212524?e=1627516800&v=beta&t=UNpLIY5KrjeAOUO7vpC5FExV_AukgNCWRQu2i6NhI_E",
+          "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
       email: "yvan@example.com",
       phoneNumber: "888 444 7676",
       facebookUrl: '',
@@ -87,7 +87,7 @@ List<Rotex> rotexList = [
       name: "Nikki Mureau",
       bio: "nikkiMureauBio",
       imageUrl:
-          "https://media-exp1.licdn.com/dms/image/C5603AQGFGdiSKuweYA/profile-displayphoto-shrink_800_800/0/1574161482971?e=1627516800&v=beta&t=Y60w592R9Q-H4Uh8n6veJoItMkRaIhzCcDn33i3vonI",
+          "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
       email: "yvan@example.com",
       phoneNumber: "888 444 7676",
       facebookUrl: '',
@@ -100,7 +100,7 @@ List<Rotex> rotexList = [
       name: "Fynn Boots",
       bio: "fynnBootsBio",
       imageUrl:
-          "https://media-exp1.licdn.com/dms/image/C4D03AQGq9LZ3qcX8cA/profile-displayphoto-shrink_800_800/0/1619609904010?e=1627516800&v=beta&t=39zQsc5RLtJ3mBS8IinEK2p7ei5KlMXJyKFqpoWoI5s",
+          "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
       email: "yvan@example.com",
       phoneNumber: "888 444 7676",
       facebookUrl: '',
@@ -139,7 +139,7 @@ List<Rotex> rotexList = [
       name: "Pascal Stomp",
       bio: "pascalStompBio",
       imageUrl:
-          "https://scontent-ams4-1.xx.fbcdn.net/v/t1.18169-9/19275222_1234391393353165_4988504603458557962_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=wd4_ftfHvIkAX8JQob7&_nc_ht=scontent-ams4-1.xx&oh=b6274ee1486a681e70dc9933b835060d&oe=60D1C6B2",
+          "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
       email: "yvan@example.com",
       phoneNumber: "888 444 7676",
       facebookUrl: '',
@@ -165,7 +165,7 @@ List<Rotex> rotexList = [
       name: "Sanne Pipping",
       bio: "sannePippingBio",
       imageUrl:
-          "https://media-exp1.licdn.com/dms/image/C4E03AQFStUO3-Hok-w/profile-displayphoto-shrink_800_800/0/1604223732978?e=1627516800&v=beta&t=FnKSLm_2a2byuSq1irStAYUxHj6AhNL16FZuwEt1WDg",
+          "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
       email: "yvan@example.com",
       phoneNumber: "888 444 7676",
       facebookUrl: '',
