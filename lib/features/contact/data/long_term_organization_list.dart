c291c6885f3174c5404cf59cd383e6d3285632b1
Ruben Talstra <RubenTalstra1211@outlook.com>
2021-08-30 12:38:54 +0200

    added more information

--- lib/features/contact/data/long_term_organization_list.dart
+++ lib/features/contact/data/long_term_organization_list.dart
@@ -22,47 +22,44 @@ List<Organization> longTermOrganizationList = [
   Organization(
     name: "Clasine Scheepers",
     bio:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        "Sinds 2017 ben ik lid van team Longterm. Ongelofelijk leuk om Nederlandse jongeren te begeleiden in hun avontuur voor jaaruitwisseling en om buitenlandse jongeren vervolgens hier in Nederland te ontvangen die een jaar lang worden ondergedompeld in hagelslag, stroopwafels en (het aller moeilijkst) de Nederlandse taal. Zo gaaf om dit elke keer met ons team te organiseren en te regelen. Begin 2021 ben ik secretaris MDJC – weer een nieuwe avontuur. \n\nIn mijn familie zijn er velen inmiddels op exchange geweest waaronder mijn man en dochter. Een van de gastgezinnen uit Amerika bezoeken we nog jaarlijks. Je krijgt er echt een nieuwe familie bij. Zelf hebben we nu drie keer een exchange student in huis gehad. Dan merk je van dichtbij waar ze tegenaan lopen in een nieuwe omgeving, andere cultuur en met een vreemde taal. Fijn om hen te helpen hun weg te vinden, een verrijking van je eigen leven.",
     imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
+        "https://www.rotary.nl/yep/yep-app/tu4w6b3-6436ie5-63h0jf-9i639i4-t3mf67-uhdrs/images/organization/long-term/clasine-schepers.png",
     email: "longtermin@rotaryyep.nl",
-    phoneNumber: "",
-    club: '',
-    district: '1570',
+    phoneNumber: null,
+    club: 'RC Huizen',
+    district: '1570 MIDDEN-NEDERLAND',
     functions: [
-      "Coordinator Long Term inbound",
-      "Coordinator Long term Middle and South America",
-      "Deputy chair Public Relations",
-      "Coordinator Europe Tour",
-      "ROTEX contact person",
+      "Secretaris MDJC",
+      "Inbound coördinator en secretaris LTEP",
     ],
   ),
   Organization(
     name: "Marga Oosterveld",
     bio:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        "Hallo, ik ben een oud uitwisselingsstudent of exchangee zoals dat heet. In het jaar 1988 – 1989 ben ik met Rotary een jaar naar het buitenland geweest. “Class of 89” noemen ze dat in Amerika. Eigenlijk wilde ik graag naar het noordoosten, maar ik kwam terecht in het zuidwesten van de USA. Totaal anders dan Nederland met indianen en cowboys, midden in de woestijn en in de bergen. Het is niet altijd makkelijk en soms best wel even slikken, maar als je dat lukt, heb je de tijd van je leven. Nu ben ik contactpersoon tussen de districten in de USA, Canada en Zuid Afrika om Rotary Clubs te vinden die willen uitwisselen met Nederland voor a lifetime in a year !!",
     imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
+        "https://www.rotary.nl/yep/yep-app/tu4w6b3-6436ie5-63h0jf-9i639i4-t3mf67-uhdrs/images/organization/long-term/marga-oosterveld.jpg",
     email: "usacanada@rotaryyep.nl",
-    phoneNumber: "",
-    club: '',
-    district: '',
+    phoneNumber: null,
+    club: 'Rotary Club Oosterwolde',
+    district: '1590 NOORD-NEDERLAND',
     functions: [
-      "Coordinator USA & Canada",
+      "Outbound coordinator USA, Canada, South Africa",
     ],
   ),
   Organization(
     name: "Judith Siebring",
     bio:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        "Mijn naam is Judith Siebring, outbound coordinator longterm RYE voor de regio’s Midden- en Zuid-Amerika. \n\nOp mijn 16e (lang geleden… ;-)) ben ik een jaar naar London, Ontario, Canada geweest op uitwisseling via de Rotary. Ik heb een fantastisch jaar gehad waar ik nog ontzettend vaak aan terugdenk. Het heeft ook veel impact gehad op latere keuzes en ervaringen in mijn leven. Reizen, andere mensen, talen en culturen leren kennen; het zit in mijn bloed. Zo heb ik tijdens mijn studie ook nog een jaar in Uruguay in Zuid-Amerika gestudeerd, en heb ik later nog in Canada en Nieuw-Zeeland gewoond en gewerkt. Inmiddels woon ik alweer een aantal jaar in Emmen en ben daar lid geworden van Rotaryclub Emmen-‘t Loo. Ik ben blij dat ik nu mijn steentje kan bijdragen aan het RYE-programma als jeugdcommissaris in mijn club, gastouder én als outbound coordinator voor de regio’s Midden- en Zuid-Amerika. Ik kijk er naar uit om jongeren een stapje op weg te helpen naar en tijdens het jaar van hun leven, met al z’n ups en downs. Daarnaast ben ik blij dat ik de kans krijg om mijn Spaans een beetje te oefenen en contacten te onderhouden met mijn ‘Rotary collega’s’ in het prachtige Zuid-Amerika.",
     imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
+        "https://www.rotary.nl/yep/yep-app/tu4w6b3-6436ie5-63h0jf-9i639i4-t3mf67-uhdrs/images/organization/long-term/judith-siebring.jpg",
     email: "msamerica@rotaryyep.nl",
-    phoneNumber: "",
-    club: '',
-    district: '',
+    phoneNumber: null,
+    club: 'RC Emmen-’t Loo',
+    district: '1590 NOORD-NEDERLAND',
     functions: [
-      "Coordinator South & Middle America",
+      "Outbound coordinator Midden- en Zuid-Amerika",
     ],
   ),
   Organization(
@@ -70,27 +67,28 @@ List<Organization> longTermOrganizationList = [
     bio:
         "Mijn naam is Ben Mureau en ik verzorg sinds enkele jaren de visa voor de inbounds en de verzekeringen voor de in- en outbounds.\n\nIn het dagelijkse leven ben ik actief als innovatie directeur bij een multi nationale onderneming, ben getrouwd en heb 3 kinderen.\n\nIk ben in de organisatie gerold toen mijn dochter ook deelnam aan het long term exchange programma. Het is werkelijk fantastisch om deel uit te mogen maken van het enthousiaste en inspirerende team dat de long term exchange organiseert en begeleidt. Samen met de Rotex’ers (oud deelnemers long term exchange) en het LT team organiseren we verschillende activiteiten en zorgen ook voor de begeleiding van de exchangers, samen met de gastouders natuurlijk.\n\nAls je beseft wat een exchange jaar voor de jongeren betekent, dan kan je alleen maar dankbaar zijn dat je daar een bijdrage aan kan en mag leveren.",
     imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
+        "https://www.rotary.nl/yep/yep-app/tu4w6b3-6436ie5-63h0jf-9i639i4-t3mf67-uhdrs/images/organization/long-term/ben-mureau.jpg",
     email: "longtermadmin@rotaryyep.nl",
     phoneNumber: null,
     club: 'RC Oldenzaal',
-    district: '1560',
+    district: '1560 OOST-NEDERLAND',
     functions: [
       "Coordinator visa and assurance Long Term Exchange",
     ],
   ),
   Organization(
-    name: "Betty Rijkmans",
+    name: "Betty Rijkmans-Siemens",
     bio:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        "Jarenlang was ik docent voortgezet onderwijs en werkte altijd met veel plezier met middelbare scholieren. Dat doe ik nu als coördinator van de Dutch Orientation Course. Long term exchangestudenten in Nederland hebben in hun uitwisselingsjaar gezamenlijke activiteiten. Een van de eerste activiteiten is de Dutch Orientation Course. Een super gezellige week ergens in Nederland waarin de studenten intensief aan de gang gaan met het leren van de  Nederlandse taal en tegelijk ook kennis maken met de Nederlandse cultuur.  De week is altijd een groot succes. De studenten leren elkaar beter kennen en naast serieus leren is er voldoende tijd om leuke dingen te doen. Allemaal even gezellig. Het team van docenten, samen met ondersteuning van Rotexers verheugen zich altijd weer op deze kennismaking.\n\nLater in het uitwisselings jaar zien we elkaar nog vaker en monitoren we hoe het gaat met iedereen. Als het jaar voorbij is hebben veel exchangers Nederland in hun hart gesloten en andersom net zo!",
     imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
+        "https://www.rotary.nl/yep/yep-app/tu4w6b3-6436ie5-63h0jf-9i639i4-t3mf67-uhdrs/images/organization/long-term/betty-rijkmans.png",
     email: "doc@rotaryyep.nl",
-    phoneNumber: "",
-    club: '',
-    district: '',
+    phoneNumber: null,
+    club: 'Rotaryclub Meppel',
+    district: '1590 NOORD-NEDERLAND',
     functions: [
       "Coordinator Dutch Orientations Course (DOC)",
+      "Inboundcouncelor noordelijke regio",
     ],
   ),
   Organization(
@@ -133,6 +131,35 @@ List<Organization> longTermOrganizationList = [
     district: '',
     functions: [
       "Teacher Orientation Course",
+    ],
+  ),
+  Organization(
+    name: "Hilleke van der Veer",
+    bio:
+        "Ik ben Hilleke van der Veer, inbound counselor Zuidelijke regio. Ik ben enthousiast geraakt over de Rotary exchange programma’s door de positieve ervaring die ik heb opgedaan als gastouder van 2 inbound uitwisselingsstudenten. Een jaar in een ander land, buiten je comfort zone. Voor de studenten een geweldig leuke en leerzame ervaring. Ook als gastouder heb ik genoten van het enthousiasme van de jongeren en dat van de vrijwilligers die zich inspannen om de uitwisselingen tot een onvergetelijke ervaring te maken. Daar wilde ik graag ook deel van uit maken. Reden voor mij om me aan te melden als vrijwilliger bij de commissie Long Term. Daar zal ik, in mijn functie van inbound counselor Zuidelijke regio, samen met de inbound counselor Noordelijke regio (Betty Rijkmans), de inbound studenten in Nederland gaan ondersteunen waar nodig. Deze functie sluit mooi aan bij mijn dagelijks werk als psycholoog. Ik heb ontzettend veel zin om er aan te beginnen!",
+    imageUrl:
+        "https://www.rotary.nl/yep/yep-app/tu4w6b3-6436ie5-63h0jf-9i639i4-t3mf67-uhdrs/images/organization/long-term/hilleke-van-der-veer.jpg",
+    email: null,
+    phoneNumber: null,
+    club: 'RC Enschede-Noord',
+    district: '1560 OOST-NEDERLAND',
+    functions: [
+      "Inbound Counselor Zuidelijke regio",
+    ],
+  ),
+  Organization(
+    name: "Patty van Vierzen",
+    bio:
+        "Ik ben Patty van Vierzen, District Jeugd Commissaris (DJC) van D1560. Ik ben getrouwd met Peter van Vierzen en wij hebben samen vier fantastische kinderen.\n\nIn 2017 ben ik gevraagd om lid te worden van Rotaryclub Ermelo. Als enthousiast vrijwilliger ben ik toegetreden tot deze geweldig mooie Rotaryclub. Het voelde meteen als een warm bad en ik heb de Rotary gedachte dan ook direct omarmd. Omdat ik mij graag op districtsniveau of landelijk niveau wilde inzetten voor Rotary ben ik in 2019 benaderd om District Jeugd Commissaris te worden van D1560. Daar heb ik volmondig JA op gezegd want door de fantastische lange en korte uitwisselingsprogramma’s van Rotary bieden wij onze jongeren de gelegenheid persoonlijk leiderschap te ontwikkelen, wereldwijde vriendschappen te sluiten, kennis te maken met andere culturen, waardoor flexibiliteit, begrip en tolerantie ontstaat en Wereldvrede binnen bereik komt te liggen. Het is een geweldige levenservaring en dat is ook mijn motivatie om mij vol enthousiasme in te zetten voor Rotary Jeugd.\n\nIn het dagelijks leven werk ik als projectassistente bij congresbureau Mark Two Academy en bij GGDNOG waar ik betrokken ben bij het vaccinatieproces inzake Covid-19.\n\nDaarnaast ben ik als bestuurslid verbonden aan stichting Ruby and Rose, een stichting die aandacht vraagt voor meer onderzoek naar en awareness voor gynaecologische kankers, www.rubyandrose.nl.\n\nMijn hobby’s zijn: verre reizen maken, skiën en wandelen",
+    imageUrl:
+        "https://www.rotary.nl/yep/yep-app/tu4w6b3-6436ie5-63h0jf-9i639i4-t3mf67-uhdrs/images/organization/long-term/patty-van-vierzen.jpg",
+    email: 'DJC1560@rotaryyep.nl',
+    phoneNumber: null,
+    club: 'Ermelo',
+    district: '1560 OOST-NEDERLAND',
+    functions: [
+      "District Jeugd Commissaris",
+      "Evenementen",
     ],
   ),
 ];
--- lib/features/contact/data/short_term_organization_list.dart
+++ lib/features/contact/data/short_term_organization_list.dart
@@ -18,11 +18,11 @@ List<Organization> shortTermOrganizationList = [
   Organization(
     name: "Femke van Houtum",
     bio:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        "Hoi, mijn naam is Femke van Houtum-Burger en ik ben de outbound coördinator Camps & Tours. Als tiener heb ik zelf aan een aantal zomerkampen deelgenomen, waardoor ik op een unieke manier heb kennisgemaakt met andere culturen en internationale vriendschappen heb opgedaan. Ik vind het heel leuk dat ik dit nu voor hedendaagse jongeren mede mogelijk kan maken!",
     imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
+        "https://www.rotary.nl/yep/yep-app/tu4w6b3-6436ie5-63h0jf-9i639i4-t3mf67-uhdrs/images/organization/short-term/femke-burger.jpg",
     email: "zomerkamp@rotaryyep.nl",
-    phoneNumber: "",
+    phoneNumber: null,
     club: '',
     district: '',
     functions: [
