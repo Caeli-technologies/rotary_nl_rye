5c6daaf656fcd7eba3a9eb6dfcc0ddaf06787a22
Jannik <pull.rebase>
2022-12-13 16:45:28 +0100

    Fix dependency injection

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,68 +1,25 @@
 // 📦 Package imports:
 import 'package:get_it/get_it.dart';
-import 'package:internet_connection_checker/internet_connection_checker.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:rotary_nl_rye/core/data/repository/exchange_students.dart';
+import 'package:rotary_nl_rye/core/data/repository/header_image_repository_impl.dart';
+import 'package:rotary_nl_rye/core/data/repository/news_repository_impl.dart';
+import 'package:rotary_nl_rye/core/data/repository/stories_repository_impl.dart';
 
 // 🌎 Project imports:
 import 'package:rotary_nl_rye/core/data/datasources/caching/shared_preferences_cache.dart';
-import 'package:rotary_nl_rye/core/network/network_info.dart';
 
 // service locator
 final sl = GetIt.instance;
 
 Future<void> init() async {
-  /// Features - Stories
-  // Bloc
-  // sl.registerFactory(() => StoriesBloc(getStories: sl()));
-
-  // Use cases
-  // sl.registerLazySingleton(() => GetStories(sl()));
-
-  // temp solluction
-  final SharedPreferencesCache cache = new SharedPreferencesCache();
-
-  await cache.invalidate();
-  // final today = DateTime(now.year, now.month, now.day);
-  // final toDaysAgo = DateTime(now.year, now.month, now.day - 2);
+  /// Data layer
 
   // Repository
-  // sl.registerLazySingleton<StoriesRepository>(() => StoriesRepositoryImpl(
-  //     storiesRemoteDataSource: sl(),
-  //     storiesLocalDataSource: sl(),
-  //     updateLocalDataSource: sl(),
-  //     networkInfo: sl()));
+  sl.registerLazySingleton(() => ExchangeStudentsRepositoryImpl(sl()));
+  sl.registerLazySingleton(() => HeaderImageRepositoryImpl());
+  sl.registerLazySingleton(() => NewsRepositoryImpl(sl()));
+  sl.registerLazySingleton(() => StoriesRepositoryImpl(sl()));
 
-  // Data Sources
-  // sl.registerLazySingleton<StoriesRemoteDataSource>(
-  //     () => StoriesRemoteDataSourceImpl(firebaseDatabase: sl()));
-  // sl.registerLazySingleton<StoriesLocalDataSource>(
-  //     () => StoriesLocalDataSourceImpl(sharedPreferences: sl()));
-  // sl.registerLazySingleton<UpdateLocalDataSource>(
-  //     () => UpdateLocalDataSourceImpl(sharedPreferences: sl()));
-
-  /// Features - Stories/Countries
-  // Bloc
-  // sl.registerFactory(() => CountriesBloc(getCountries: sl()));
-
-  // Use cases
-  // sl.registerLazySingleton(() => GetCountries(sl()));
-
-  // Repository
-  // sl.registerLazySingleton<CountriesRepository>(() => CountriesRepositoryImpl(
-  //     countriesRemoteDataSource: sl()));
-
-  // Data Sources
-  // sl.registerLazySingleton<CountriesRemoteDataSource>(
-  //         () => CountriesRemoteDataSourceImpl());
-
-  /// Core
-  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
-
-  /// External
-  final sharedPreferences = await SharedPreferences.getInstance();
-  sl.registerLazySingleton(() => sharedPreferences);
-  sl.registerLazySingleton(() => InternetConnectionChecker());
-
-  // final FirebaseDatabase firebaseDatabase = FirebaseDatabase();
-  // sl.registerLazySingleton(() => firebaseDatabase);
+  // Cache
+  sl.registerLazySingleton(() => SharedPreferencesCache());
 }
